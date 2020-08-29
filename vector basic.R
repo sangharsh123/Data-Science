5a1957ecce8e5fabf73dfe44df89e8e4084f2ed9
sangharsh123 <70280012+sangharsh123@users.noreply.github.com>
2020-08-29 22:47:24 +0530

    Add files via upload

---
+++ vector basic.R
@@ -0,0 +1,18 @@
+#create vector
+vect_x <-c(1,2,3,4)
+vect_y <-c(2,4,6,8)
+ # Add vector
+Add_vect <-vect_x+vect_y
+Add_vect*5
+# Accesing the element
+vect_y[2]
+vect_x[3]<-6
+vect_y[3]<-10
+vect_y[2]<- -12 
+vect_x[1]<- -11
+#print data
+my_char<- c('cat','dog',10,12)
+my_car<- c('sangharsh','engineer','mech','mumbai','university')
+
+
+
