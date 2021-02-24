f9707179b54f4ba493d13c88352d722b1263e197
sangharsh123 <70280012+sangharsh123@users.noreply.github.com>
2021-02-24 10:46:25 +0530

    Add files via upload

---
+++ basic.R
@@ -0,0 +1,20 @@
+db_set1<- c(33,4,1,1,3,1,2)
+print("welcome")
+x<-c 2,4,7
+df<-data.frame(x=1:3, y= c("a","b","c"))
+df[1]
+ set1<- data.frame(x=1:5, y=c("A","B","C","D","E"),z=c("akshay","nilu","deeru","sandy","lucky"), Q=c("andy", "bendy", "sandy","candy","peeru"))              
+               tab1<- data.frame(X=1:2,y=c("neeru","ammi"),z=c("A","B"))
+      vect_char<- c("R","PYTHON")         
+      Chaar_1<- c("java","sandy")
+      df1<-c (2,3,4,5)*2
+df_new<-df1+1      
+df_new[2]*4+1
+tab2 <- data.frame(height cm =c(20,30), weight kg=c(69 ,90))
+set1[2,2]
+set1[2,3]
+set1[3,3]
+set1[4,4]
+set1[3,4]
+set1[5,3]
+set1[c(2,3,4),c(3,4,2)]
